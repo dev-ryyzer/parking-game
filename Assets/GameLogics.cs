ac7bbbeb3b440f7e79fad1014055939d3e3c852e
talhanmughal <53786079+talhanmughal@users.noreply.github.com>
2022-06-02 11:24:11 +0500

    First Commit

---
+++ Assets/GameLogics.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogics : MonoBehaviour
+{
+    [SerializeField] private GameObject ParkingPlace;
+    [SerializeField] private GameObject SuccessPanel;
+    [SerializeField] private GameObject ParkButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Renderer ParkingPlace_parrender = ParkingPlace.GetComponent<Renderer>();
+        ParkingPlace_parrender.material.color = Color.green;
+
+
+        ParkButton.SetActive(true);
+        if (other.gameObject.tag == "ParkingPlace")
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Renderer ParkingPlace_parrender = ParkingPlace.GetComponent<Renderer>();
+        ParkingPlace_parrender.material.color = Color.white;
+        ParkButton.SetActive(false);
+
+        if (other.gameObject.tag == "ParkingPlace")
+        {
+           
+        }
+    }
+
+    public void ParkButtonSuccess()
+    {
+        SuccessPanel.SetActive(true);
+        ParkButton.SetActive(false);
+
+    }
+}
---
+++ Assets/RealisticCarControllerV3/Scripts/RCC_Spawner.cs
@@ -0,0 +1,38 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2014 - 2020 BoneCracker Games
+// http://www.bonecrackergames.com
+// Buğra Özdoğanlar
+//
+//----------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCC_Spawner : MonoBehaviour {
+
+	public GameObject[] LevelSpawnPositions;
+	int LevelCounter=0;
+
+	private Vector3 SpawnPosition;
+	private Quaternion SpawnRotation;
+	// Use this for initialization
+	void Start () {
+
+		int selectedIndex = PlayerPrefs.GetInt ("SelectedRCCVehicle", 0);
+		RCC.SpawnRCC (RCC_DemoVehicles.Instance.vehicles [selectedIndex], transform.position, transform.rotation, true, true, true);
+
+	}
+
+	public void SpawnForNextLevel()
+    {
+		Destroy(GameObject.FindWithTag("Player"));
+		LevelCounter++;
+		SpawnPosition = LevelSpawnPositions[LevelCounter].transform.position;
+		SpawnRotation = LevelSpawnPositions[LevelCounter].transform.rotation;
+		int selectedIndex = PlayerPrefs.GetInt("SelectedRCCVehicle", 0);
+		RCC.SpawnRCC(RCC_DemoVehicles.Instance.vehicles[selectedIndex], SpawnPosition , SpawnRotation, true, true, true);
+	}
+}
